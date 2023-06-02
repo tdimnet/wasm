862fc3d099f958a406cad23c99ae2d2805b6c24e
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-02 10:44:10 +0200

    Calling minusOne function

--- 02-iwasm/app.js
+++ 02-iwasm/app.js
@@ -1,5 +1,6 @@
 import {
-  add
+  add,
+  minusOne
 } from './build/release.js'
 
 function addingNumbers() {
@@ -7,8 +8,14 @@ function addingNumbers() {
   $addNumbers.innerHTML = `Adding 1 and 2 equals ${add(1, 2)}` 
 }
 
+function subbingByOne() {
+  const $minusOne = document.querySelector('.minus-one')
+  $minusOne.innerHTML = `Substracting a 4 by 1 equals ${minusOne(4)}`
+}
+
 function main() {
   addingNumbers()
+  subbingByOne()
 }
 
 export default main
